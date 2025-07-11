46afa671505b941df4fc6cb62114dccff1de8359
satvik-A <satvikaderla@gmail.com>
2025-07-12 00:15:20 +0530

    f

--- mainswipe.py
+++ mainswipe.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, HTTPException, Request
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from supabase import create_client, Client
 from dotenv import load_dotenv
